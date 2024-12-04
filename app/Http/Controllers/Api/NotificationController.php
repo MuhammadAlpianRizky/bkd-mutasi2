0d44f05a52c094685e9adf033ec4bc6ee0d39359
Muhammad Alpian Rizky <alpianrizky918@gmail.com>
2024-12-04 10:31:13 +0700

    API dari laravel ke Nodejs sudah bisa

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -12,7 +12,7 @@ class NotificationController extends Controller
 {
     // Ambil data notifikasi dengan is_wa = 0, hanya kolom Nama, no_hp, is_wa, dan message
     $pendingNotifications = NotifWa::where('is_wa', 0)
-        ->select('nama', 'no_hp', 'is_wa', 'message','status') // Pilih kolom yang diinginkan
+        ->select('id','nama', 'no_hp', 'is_wa', 'message','status') // Pilih kolom yang diinginkan
         ->get();
 
     // Kembalikan response berupa data notifikasi
@@ -22,30 +22,26 @@ class NotificationController extends Controller
     ]);
 }
 public function updateStatus(Request $request)
-    {
-        
-        // Validasi data
-        $request->validate([
-            'number' => 'required|string',
-            'message' => 'required|string',
-            'status' => 'required|in:sent',
-        ]);
+{
+    
+    $request->validate([
+        'number' => 'required|string',
+        'message' => 'required|string',
+        'status' => 'required|in:sent',
+        'id' => 'required|integer',
+    ]);
 
-        // Mengubah nomor telepon ke format internasional (misalnya +62)
     $number = $request->number;
 
-    // Jika nomor mulai dengan '08', ubah menjadi '+62'
     if (substr($number, 0, 2) == '08') {
         $number = '+62' . substr($number, 2);
     }
 
-    // Perbarui status is_wa menjadi 1 jika sudah terkirim
-    $notif = NotifWa::where('no_hp', $number) // Gunakan nomor yang sudah diformat
-                    ->where('message', $request->message)
-                    ->first();
+    // Mencari data berdasarkan ID
+    $notif = NotifWa::where('id', $request->id)->first();
 
     if ($notif) {
-        $notif->is_wa = 1; // Mengubah status menjadi 1 (sent)
+        $notif->is_wa = 1; // Ubah status menjadi 'sent'
         $notif->save();
     }
 
@@ -53,5 +49,6 @@ public function updateStatus(Request $request)
         'success' => true,
         'message' => 'Status pesan diperbarui.',
     ]);
-    }
+}
+
 }
